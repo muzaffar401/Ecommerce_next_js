ef2d44d2d6cb8d05ce244acc7ff977cae5552922
muzaffar401 <ma9400667@gmail.com>
2024-12-22 02:05:44 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CartProvider>
-          <Navbar />
           <div>{children}</div>
         </CartProvider>
 
