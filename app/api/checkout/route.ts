2eac54019b615df7208c31b6c9847565496a52f9
muzaffar401 <ma9400667@gmail.com>
2025-01-11 05:07:45 -0800

    ECOMMERCE-WEBSITE

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -56,8 +56,8 @@ export const POST = async (request:any) => {
      const session = await stripe.checkout.sessions.create({
         line_items:stripeProducts,
         mode: 'payment',
-        success_url: `http://localhost:3000//success`,
-        cancel_url: `http://localhost:3000/`,
+        success_url: `https://ecommerce-next-js-2947.vercel.app/success`,
+        cancel_url: `https://ecommerce-next-js-2947.vercel.app/`,
       });
       
     return NextResponse.json({
